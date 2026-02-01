c1a586506c18ae54e35a752febc021509bff2be8
Harmanpreet Singh <preetsinghharman27@gmail.com>
2026-02-01 13:07:04 -0500

    fix macos build error

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -6,4 +6,15 @@ class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
+
+  override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+    return true
+  }
+  override func applicationDidFinishLaunching(_ aNotification: Notification) {
+    let controller = mainFlutterWindow?.contentViewController as! FlutterViewController
+    let registrar = controller.registrar(forPlugin: "ForegroundWindowPlugin")
+    
+    // Add this line:
+    ForegroundWindowPlugin.register(with: registrar)
+  }
 }
